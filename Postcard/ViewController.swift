1fe9dab0175d20be6b9178d983973c81b2da5088
localtable <zach@localtable.co>
2015-05-21 18:05:04 -0400

    Added nameLabel

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    @IBOutlet weak var nameLabel: UILabel!
+
     
     
     
@@ -28,9 +30,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func sendMailButtonPressed(sender: UIButton) {
-        messageLabel.hidden = false
-        messageLabel.text   = enterMessageTextField.text
-        messageLabel.textColor = UIColor.redColor()
+        //Just making a comment
+        messageLabel.hidden     = false
+        messageLabel.text       = enterMessageTextField.text
+        messageLabel.textColor  = UIColor.redColor()
+        
+        nameLabel.hidden = false
+        nameLabel.text   = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
         
         enterMessageTextField.text = ""
         enterMessageTextField.resignFirstResponder()
